6900395b97c6bf09006a0f7901cdb5821bca580d
freephoenix888 <freephoenix888@gmail.com>
2023-11-14 08:42:43 +0000

    Generate new package class

--- src/package.ts
+++ src/package.ts
@@ -18,12 +18,13 @@ const {name: packageName} = package;
 #### Use id method to get the id of the link
 ```ts
 const package = new Package({deep});
+const parseItInsertHandlerCodeLinkId = await package["parseItInsertHandlerCode"].id();
 const propertiesTreeLinkId = await package["propertiesTree"].id();
+const treeIncludeFromCurrentBooleanLinkId = await package["treeIncludeFromCurrentBoolean"].id();
 const hasResultTypeLinkId = await package["HasResult"].id();
 const treeIncludeDownHasResultLinkId = await package["treeIncludeDownHasResult"].id();
 const rootTypeLinkId = await package["Root"].id();
 const rootValueLinkId = await package["rootValue"].id();
-const parseItInsertHandlerTranspiledCodeLinkId = await package["parseItInsertHandlerTranspiledCode"].id();
 const dependency@deep-foundation/booleanLinkId = await package["dependency@deep-foundation/boolean"].id();
 const booleanTypeLinkId = await package["Boolean"].id();
 const objectTypeLinkId = await package["Object"].id();
@@ -33,25 +34,25 @@ const treeIncludeFromCurrentNumberLinkId = await package["treeIncludeFromCurrent
 const stringTypeLinkId = await package["String"].id();
 const treeIncludeFromCurrentStringLinkId = await package["treeIncludeFromCurrentString"].id();
 const parseItTypeLinkId = await package["ParseIt"].id();
-const treeIncludeFromCurrentBooleanLinkId = await package["treeIncludeFromCurrentBoolean"].id();
-const clientHandlerCodeLinkId = await package["clientHandlerCode"].id();
-const clientHandlerLinkId = await package["clientHandler"].id();
+const parseItInsertHandlerTranspiledCodeLinkId = await package["parseItInsertHandlerTranspiledCode"].id();
 const parseItInsertHandlerLinkId = await package["parseItInsertHandler"].id();
 const handleParseItInsertLinkId = await package["handleParseItInsert"].id();
-const parseItInsertHandlerCodeLinkId = await package["parseItInsertHandlerCode"].id();
 const parseItInsertHandlerTranspiledCodeGeneratedFromLinkId = await package["parseItInsertHandlerTranspiledCodeGeneratedFrom"].id();
+const clientHandlerCodeLinkId = await package["clientHandlerCode"].id();
+const clientHandlerLinkId = await package["clientHandler"].id();
 ```
 
 #### Use idLocal method to get the local id of the link
 ```ts
 const package = new Package({deep});
 await package.applyMinilinks();
+const parseItInsertHandlerCodeLinkId = package["parseItInsertHandlerCode"].idLocal();
 const propertiesTreeLinkId = package["propertiesTree"].idLocal();
+const treeIncludeFromCurrentBooleanLinkId = package["treeIncludeFromCurrentBoolean"].idLocal();
 const hasResultTypeLinkId = package["HasResult"].idLocal();
 const treeIncludeDownHasResultLinkId = package["treeIncludeDownHasResult"].idLocal();
 const rootTypeLinkId = package["Root"].idLocal();
 const rootValueLinkId = package["rootValue"].idLocal();
-const parseItInsertHandlerTranspiledCodeLinkId = package["parseItInsertHandlerTranspiledCode"].idLocal();
 const dependency@deep-foundation/booleanLinkId = package["dependency@deep-foundation/boolean"].idLocal();
 const booleanTypeLinkId = package["Boolean"].idLocal();
 const objectTypeLinkId = package["Object"].idLocal();
@@ -61,23 +62,23 @@ const treeIncludeFromCurrentNumberLinkId = package["treeIncludeFromCurrentNumber
 const stringTypeLinkId = package["String"].idLocal();
 const treeIncludeFromCurrentStringLinkId = package["treeIncludeFromCurrentString"].idLocal();
 const parseItTypeLinkId = package["ParseIt"].idLocal();
-const treeIncludeFromCurrentBooleanLinkId = package["treeIncludeFromCurrentBoolean"].idLocal();
-const clientHandlerCodeLinkId = package["clientHandlerCode"].idLocal();
-const clientHandlerLinkId = package["clientHandler"].idLocal();
+const parseItInsertHandlerTranspiledCodeLinkId = package["parseItInsertHandlerTranspiledCode"].idLocal();
 const parseItInsertHandlerLinkId = package["parseItInsertHandler"].idLocal();
 const handleParseItInsertLinkId = package["handleParseItInsert"].idLocal();
-const parseItInsertHandlerCodeLinkId = package["parseItInsertHandlerCode"].idLocal();
 const parseItInsertHandlerTranspiledCodeGeneratedFromLinkId = package["parseItInsertHandlerTranspiledCodeGeneratedFrom"].idLocal();
+const clientHandlerCodeLinkId = package["clientHandlerCode"].idLocal();
+const clientHandlerLinkId = package["clientHandler"].idLocal();
 ```
 #### Use name field to get the name of the link
 ```ts
 const package = new Package({deep});
+const parseItInsertHandlerCode = package["parseItInsertHandlerCode"].name;
 const propertiesTree = package["propertiesTree"].name;
+const treeIncludeFromCurrentBoolean = package["treeIncludeFromCurrentBoolean"].name;
 const hasResult = package["HasResult"].name;
 const treeIncludeDownHasResult = package["treeIncludeDownHasResult"].name;
 const root = package["Root"].name;
 const rootValue = package["rootValue"].name;
-const parseItInsertHandlerTranspiledCode = package["parseItInsertHandlerTranspiledCode"].name;
 const dependency@deep-foundation/boolean = package["dependency@deep-foundation/boolean"].name;
 const boolean = package["Boolean"].name;
 const object = package["Object"].name;
@@ -87,13 +88,12 @@ const treeIncludeFromCurrentNumber = package["treeIncludeFromCurrentNumber"].nam
 const string = package["String"].name;
 const treeIncludeFromCurrentString = package["treeIncludeFromCurrentString"].name;
 const parseIt = package["ParseIt"].name;
-const treeIncludeFromCurrentBoolean = package["treeIncludeFromCurrentBoolean"].name;
-const clientHandlerCode = package["clientHandlerCode"].name;
-const clientHandler = package["clientHandler"].name;
+const parseItInsertHandlerTranspiledCode = package["parseItInsertHandlerTranspiledCode"].name;
 const parseItInsertHandler = package["parseItInsertHandler"].name;
 const handleParseItInsert = package["handleParseItInsert"].name;
-const parseItInsertHandlerCode = package["parseItInsertHandlerCode"].name;
 const parseItInsertHandlerTranspiledCodeGeneratedFrom = package["parseItInsertHandlerTranspiledCodeGeneratedFrom"].name;
+const clientHandlerCode = package["clientHandlerCode"].name;
+const clientHandler = package["clientHandler"].name;
 ```
 */
 export class Package extends BasePackage {
@@ -104,6 +104,27 @@ export class Package extends BasePackage {
     });
   }
 
+  /**
+      @example
+      #### Use id method to get the id of the parseItInsertHandlerCode link
+      ```ts
+      const package = new Package({deep});
+      const parseItInsertHandlerCodeLinkId = await package["parseItInsertHandlerCode"].id();
+      ```
+      #### Use localId method to get the local id of the parseItInsertHandlerCode link
+      ```ts
+      const package = new Package({deep});
+      const parseItInsertHandlerCodeLinkId = await package["parseItInsertHandlerCode"].localId();
+      ```
+      #### Use name field to get the name of the parseItInsertHandlerCode link
+      ```ts
+      const package = new Package({deep});
+      const parseItInsertHandlerCode = await package["parseItInsertHandlerCode"].name;
+      ```
+      */
+  public "parseItInsertHandlerCode" = this.createEntity(
+    "parseItInsertHandlerCode",
+  );
   /**
       @example
       #### Use id method to get the id of the propertiesTree link
@@ -123,6 +144,27 @@ export class Package extends BasePackage {
       ```
       */
   public "propertiesTree" = this.createEntity("propertiesTree");
+  /**
+      @example
+      #### Use id method to get the id of the treeIncludeFromCurrentBoolean link
+      ```ts
+      const package = new Package({deep});
+      const treeIncludeFromCurrentBooleanLinkId = await package["treeIncludeFromCurrentBoolean"].id();
+      ```
+      #### Use localId method to get the local id of the treeIncludeFromCurrentBoolean link
+      ```ts
+      const package = new Package({deep});
+      const treeIncludeFromCurrentBooleanLinkId = await package["treeIncludeFromCurrentBoolean"].localId();
+      ```
+      #### Use name field to get the name of the treeIncludeFromCurrentBoolean link
+      ```ts
+      const package = new Package({deep});
+      const treeIncludeFromCurrentBoolean = await package["treeIncludeFromCurrentBoolean"].name;
+      ```
+      */
+  public "treeIncludeFromCurrentBoolean" = this.createEntity(
+    "treeIncludeFromCurrentBoolean",
+  );
   /**
       @example
       #### Use id method to get the id of the HasResult link
@@ -201,27 +243,6 @@ export class Package extends BasePackage {
       ```
       */
   public "rootValue" = this.createEntity("rootValue");
-  /**
-      @example
-      #### Use id method to get the id of the parseItInsertHandlerTranspiledCode link
-      ```ts
-      const package = new Package({deep});
-      const parseItInsertHandlerTranspiledCodeLinkId = await package["parseItInsertHandlerTranspiledCode"].id();
-      ```
-      #### Use localId method to get the local id of the parseItInsertHandlerTranspiledCode link
-      ```ts
-      const package = new Package({deep});
-      const parseItInsertHandlerTranspiledCodeLinkId = await package["parseItInsertHandlerTranspiledCode"].localId();
-      ```
-      #### Use name field to get the name of the parseItInsertHandlerTranspiledCode link
-      ```ts
-      const package = new Package({deep});
-      const parseItInsertHandlerTranspiledCode = await package["parseItInsertHandlerTranspiledCode"].name;
-      ```
-      */
-  public "parseItInsertHandlerTranspiledCode" = this.createEntity(
-    "parseItInsertHandlerTranspiledCode",
-  );
   /**
       @example
       #### Use id method to get the id of the dependency@deep-foundation/boolean link
@@ -403,63 +424,25 @@ export class Package extends BasePackage {
   public "ParseIt" = this.createEntity("ParseIt");
   /**
       @example
-      #### Use id method to get the id of the treeIncludeFromCurrentBoolean link
+      #### Use id method to get the id of the parseItInsertHandlerTranspiledCode link
       ```ts
       const package = new Package({deep});
-      const treeIncludeFromCurrentBooleanLinkId = await package["treeIncludeFromCurrentBoolean"].id();
+      const parseItInsertHandlerTranspiledCodeLinkId = await package["parseItInsertHandlerTranspiledCode"].id();
       ```
-      #### Use localId method to get the local id of the treeIncludeFromCurrentBoolean link
+      #### Use localId method to get the local id of the parseItInsertHandlerTranspiledCode link
       ```ts
       const package = new Package({deep});
-      const treeIncludeFromCurrentBooleanLinkId = await package["treeIncludeFromCurrentBoolean"].localId();
+      const parseItInsertHandlerTranspiledCodeLinkId = await package["parseItInsertHandlerTranspiledCode"].localId();
       ```
-      #### Use name field to get the name of the treeIncludeFromCurrentBoolean link
+      #### Use name field to get the name of the parseItInsertHandlerTranspiledCode link
       ```ts
       const package = new Package({deep});
-      const treeIncludeFromCurrentBoolean = await package["treeIncludeFromCurrentBoolean"].name;
+      const parseItInsertHandlerTranspiledCode = await package["parseItInsertHandlerTranspiledCode"].name;
       ```
       */
-  public "treeIncludeFromCurrentBoolean" = this.createEntity(
-    "treeIncludeFromCurrentBoolean",
+  public "parseItInsertHandlerTranspiledCode" = this.createEntity(
+    "parseItInsertHandlerTranspiledCode",
   );
-  /**
-      @example
-      #### Use id method to get the id of the clientHandlerCode link
-      ```ts
-      const package = new Package({deep});
-      const clientHandlerCodeLinkId = await package["clientHandlerCode"].id();
-      ```
-      #### Use localId method to get the local id of the clientHandlerCode link
-      ```ts
-      const package = new Package({deep});
-      const clientHandlerCodeLinkId = await package["clientHandlerCode"].localId();
-      ```
-      #### Use name field to get the name of the clientHandlerCode link
-      ```ts
-      const package = new Package({deep});
-      const clientHandlerCode = await package["clientHandlerCode"].name;
-      ```
-      */
-  public "clientHandlerCode" = this.createEntity("clientHandlerCode");
-  /**
-      @example
-      #### Use id method to get the id of the clientHandler link
-      ```ts
-      const package = new Package({deep});
-      const clientHandlerLinkId = await package["clientHandler"].id();
-      ```
-      #### Use localId method to get the local id of the clientHandler link
-      ```ts
-      const package = new Package({deep});
-      const clientHandlerLinkId = await package["clientHandler"].localId();
-      ```
-      #### Use name field to get the name of the clientHandler link
-      ```ts
-      const package = new Package({deep});
-      const clientHandler = await package["clientHandler"].name;
-      ```
-      */
-  public "clientHandler" = this.createEntity("clientHandler");
   /**
       @example
       #### Use id method to get the id of the parseItInsertHandler link
@@ -500,27 +483,6 @@ export class Package extends BasePackage {
   public "handleParseItInsert" = this.createEntity("handleParseItInsert");
   /**
       @example
-      #### Use id method to get the id of the parseItInsertHandlerCode link
-      ```ts
-      const package = new Package({deep});
-      const parseItInsertHandlerCodeLinkId = await package["parseItInsertHandlerCode"].id();
-      ```
-      #### Use localId method to get the local id of the parseItInsertHandlerCode link
-      ```ts
-      const package = new Package({deep});
-      const parseItInsertHandlerCodeLinkId = await package["parseItInsertHandlerCode"].localId();
-      ```
-      #### Use name field to get the name of the parseItInsertHandlerCode link
-      ```ts
-      const package = new Package({deep});
-      const parseItInsertHandlerCode = await package["parseItInsertHandlerCode"].name;
-      ```
-      */
-  public "parseItInsertHandlerCode" = this.createEntity(
-    "parseItInsertHandlerCode",
-  );
-  /**
-      @example
       #### Use id method to get the id of the parseItInsertHandlerTranspiledCodeGeneratedFrom link
       ```ts
       const package = new Package({deep});
@@ -540,6 +502,44 @@ export class Package extends BasePackage {
   public "parseItInsertHandlerTranspiledCodeGeneratedFrom" = this.createEntity(
     "parseItInsertHandlerTranspiledCodeGeneratedFrom",
   );
+  /**
+      @example
+      #### Use id method to get the id of the clientHandlerCode link
+      ```ts
+      const package = new Package({deep});
+      const clientHandlerCodeLinkId = await package["clientHandlerCode"].id();
+      ```
+      #### Use localId method to get the local id of the clientHandlerCode link
+      ```ts
+      const package = new Package({deep});
+      const clientHandlerCodeLinkId = await package["clientHandlerCode"].localId();
+      ```
+      #### Use name field to get the name of the clientHandlerCode link
+      ```ts
+      const package = new Package({deep});
+      const clientHandlerCode = await package["clientHandlerCode"].name;
+      ```
+      */
+  public "clientHandlerCode" = this.createEntity("clientHandlerCode");
+  /**
+      @example
+      #### Use id method to get the id of the clientHandler link
+      ```ts
+      const package = new Package({deep});
+      const clientHandlerLinkId = await package["clientHandler"].id();
+      ```
+      #### Use localId method to get the local id of the clientHandler link
+      ```ts
+      const package = new Package({deep});
+      const clientHandlerLinkId = await package["clientHandler"].localId();
+      ```
+      #### Use name field to get the name of the clientHandler link
+      ```ts
+      const package = new Package({deep});
+      const clientHandler = await package["clientHandler"].name;
+      ```
+      */
+  public "clientHandler" = this.createEntity("clientHandler");
 }
 
 export type PackageOptions = Omit<BasePackageOptions, "name">;
