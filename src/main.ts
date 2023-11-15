684d580bdea74ce35c6abe869d2e6c9db9e311a2
FreePhoenix888 <freephoenix888@gmail.com>
2023-11-15 13:56:32 +0600

    Exportt allowed values, remove Obj

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ export {
   ObjectToLinksConverterDecorator,
   createObjectToLinksConverterDecorator,
 } from "./create-object-to-links-converter-decorator.js";
-export { Obj } from "./obj.js";
+export {AllowedArray,AllowedObject,AllowedPrimitive,AllowedValue} from './allowed-values.js'
 export { PACKAGE_NAME } from "./package-name.js";
 export { Package, PackageOptions } from "./package.js";
 export { REQUIRED_PACKAGES_IN_MINILINKS } from "./required-packages-in-minilinks.js";
